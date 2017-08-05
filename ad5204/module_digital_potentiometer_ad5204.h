45419b487231a9858cb9acc715407e43bba010cb
Vadim Deryabkin <deryabkinda@mail.ru>
2017-08-05 22:51:50 +0700

    В процессе написания ad5204 (начал описывать конфиг-структуру).

--- ad5204/module_digital_potentiometer_ad5204.h
+++ ad5204/module_digital_potentiometer_ad5204.h
@@ -1,3 +1,13 @@
+#include "user_os.h"
+#include "mk_hardware_interfaces_spi.h"
+
+struct ad5204_struct_cfg_t {
+    spi_master_8bit_base*       spi;
+    USER_OS_STATIC_MUTEX*       mutex;      // Для предотвращения коллизия на шине SPI.
+
+};
+
+template < uint8_t COUNT >              // Колличество потенциометров на шине SPI.
 class ad5204 {
 
 };
