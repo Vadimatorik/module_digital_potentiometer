1285d547d165902e2962075d67fc9a5fa196304c
Vadimatorik <Vadimatorikda@gmail.com>
2018-05-09 16:53:21 +0700

    Добавил условную компиляюцию по define.

--- ad5204/module_digital_potentiometer_ad5204.h
+++ ad5204/module_digital_potentiometer_ad5204.h
@@ -1,26 +1,32 @@
+#pragma once
+
+#ifdef MODULE_DIGITAL_POTENTIOMETER_ENABLED
+
 #include "module_digital_potentiometer_ad5204_struct.h"
 
 enum class EC_AD5204_ANSWER {
-    OK              = 0,
-    ARG_ERROR       = 1,
+	OK				= 0,
+	ARG_ERROR		= 1,
 	SPI_ERROR		= 2
 };
 
-template < uint8_t COUNT >              // Колличество потенциометров на шине SPI.
+template < uint8_t COUNT >				// Колличество потенциометров на шине SPI.
 class ad5204 {
 public:
-    constexpr ad5204 ( ad5204_struct_cfg_t* cfg );
+	constexpr ad5204 ( ad5204_struct_cfg_t* cfg );
 
-    void    connect_on     ( void );
-    void    connect_off    ( void );
+	void	connect_on	 ( void );
+	void	connect_off	( void );
 
-    EC_AD5204_ANSWER    value_set           ( uint8_t chip_number, uint8_t reg, uint8_t value );
-    EC_AD5204_ANSWER    value_read          ( uint8_t chip_number, uint8_t reg, uint8_t& out_result_data );
+	EC_AD5204_ANSWER	value_set			( uint8_t chip_number, uint8_t reg, uint8_t value );
+	EC_AD5204_ANSWER	value_read			( uint8_t chip_number, uint8_t reg, uint8_t& out_result_data );
 
 private:
-    const ad5204_struct_cfg_t*          const cfg;
-    // Номер чипа, данные каналов.
-    mutable uint8_t buf[ COUNT ][ 4 ] = { 0 };   // Состояние каждого регистра каждого потенциометра.
+	const ad5204_struct_cfg_t*			const cfg;
+	// Номер чипа, данные каналов.
+	mutable uint8_t buf[ COUNT ][ 4 ] = { 0 };	// Состояние каждого регистра каждого потенциометра.
 };
 
 #include "module_digital_potentiometer_ad5204_func.h"
+
+#endif
--- ad5204/module_digital_potentiometer_ad5204_struct.h
+++ ad5204/module_digital_potentiometer_ad5204_struct.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#ifdef MODULE_DIGITAL_POTENTIOMETER_ENABLED
+
 #include "user_os.h"
 #include "mc_hardware_interfaces_spi.h"
 #include "mc_hardware_interfaces_pin.h"
 
 struct ad5204_struct_cfg_t {
-	SpiMaster8BitBase*	   const spi;
-	USER_OS_STATIC_MUTEX*	   const mutex;		// Для предотвращения коллизия на шине SPI.
-	PinBase*				   const cs;		// Выбор чипа(-ов). Активный 0.
-	PinBase*				   const shdn;		// Прямое включение (см. документацию).
+	SpiMaster8BitBase*			const spi;
+	USER_OS_STATIC_MUTEX*		const mutex;		// Для предотвращения коллизия на шине SPI.
+	PinBase*					const cs;			// Выбор чипа(-ов). Активный 0.
+	PinBase*					const shdn;			// Прямое включение (см. документацию).
 };
+
+#endif
